a991cb5aa80712d7e256be6a6950541228b17de1
vphat19522003 <19522003@gm.uit.edu.vn>
2023-11-07 17:29:59 +0700

    add

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -142,7 +142,7 @@ export async function fetchInvoicesPages(query: string) {
       invoices.date::text ILIKE ${`%${query}%`} OR
       invoices.status ILIKE ${`%${query}%`}
   `;
-
+    console.log({ count });
     const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
     return totalPages;
   } catch (error) {
